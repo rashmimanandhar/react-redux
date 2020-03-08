b268deb6654c72936b96422ea3f2c6ec67381476
Rashmi <rashmi.manandhar7@gmail.com>
2020-03-08 12:46:48 -0400

    feat: move actions from simple html created to respective files

---
+++ goals-todos/src/actions/shared.js
@@ -0,0 +1,21 @@
+import API from "goals-todos-api";
+
+const RECEIVE_DATA = "RECEIVE_DATA";
+
+function receiveData(todos, goals) {
+  return {
+    type: RECEIVE_DATA,
+    todos,
+    goals
+  };
+}
+
+export function handleInitialData() {
+  return dispatch => {
+    return Promise.all([API.fetchGoals(), API.fetchTodos()]).then(
+      ([goals, todos]) => {
+        dispatch(receiveData(todos, goals));
+      }
+    );
+  };
+}
